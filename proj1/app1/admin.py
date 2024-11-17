e81ab3382c5f134a441bef80e78fe53bf1f56fae
subashsubash7010 <subashsubash7010@gmail.com>
2024-11-17 22:47:30 +0530

    success

---
+++ proj1/app1/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import bankloan,userAdmin
+admin.site.register(bankloan,userAdmin)
+
+# Register your models here.
---
+++ proj1/app1/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib import admin
+class bankloan(models.Model):
+    customer_name=models.CharField(max_length=100,primary_key=True)
+    customer_id=models.CharField(max_length=100)
+    loan_no=models.IntegerField()
+    loan_amount=models.IntegerField()
+    email=models.EmailField()
+ 
+class userAdmin(admin.ModelAdmin):
+    list_display=('customer_name','customer_id','loan_no','loan_amount','email')
+# Create your models here.
